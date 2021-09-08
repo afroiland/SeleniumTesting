85c5392ed8b3dc8a5fa10978d4f9179d93646a59
afroiland <andrew.froiland@gmail.com>
2021-09-08 16:07:21 -0500

    Remove ignored files

--- SeleniumTest2/UnitTest1.cs
+++ SeleniumTest2/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace SeleniumTest2
 {
@@ -19,14 +20,24 @@ namespace SeleniumTest2
             IWebDriver webDriver = new ChromeDriver();
             webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
 
+            webDriver.Manage().Window.Maximize();
+
             IWebElement lnkLogin = webDriver.FindElement(By.Id("loginLink"));
             lnkLogin.Click();
 
             var txtUserName = webDriver.FindElement(By.Name("UserName"));
+            var txtPassword = webDriver.FindElement(By.Name("Password"));
+            var loginButton = webDriver.FindElement(By.ClassName("btn-default"));
 
             Assert.That(txtUserName.Displayed, Is.True);
+            Assert.That(txtPassword.Displayed, Is.True);
+            Assert.That(loginButton.Displayed, Is.True);
 
             txtUserName.SendKeys("admin");
+            txtPassword.SendKeys("password");
+            //webDriver.FindElement(By.Id("RememberMe")).Click();
+            //Thread.Sleep(5000);
+            loginButton.Click();
         }
     }
 }
