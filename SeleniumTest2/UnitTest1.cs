04ab0763d27c244b68c09a795a22112d07393fd9
afroiland <andrew.froiland@gmail.com>
2021-09-10 13:32:28 -0500

    use variety of locators

--- SeleniumTest2/UnitTest1.cs
+++ SeleniumTest2/UnitTest1.cs
@@ -17,26 +17,35 @@ namespace SeleniumTest2
         [Test]
         public void Test1()
         {
-            IWebDriver webDriver = new ChromeDriver();
-            webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            //driver.Navigate().GoToUrl("http://localhost:3000/");
 
-            webDriver.Manage().Window.Maximize();
+            driver.Manage().Window.Maximize();
 
-            IWebElement lnkLogin = webDriver.FindElement(By.Id("loginLink"));
+            IWebElement lnkLogin = driver.FindElement(By.Id("loginLink"));
             lnkLogin.Click();
 
-            var txtUserName = webDriver.FindElement(By.Name("UserName"));
-            var txtPassword = webDriver.FindElement(By.Name("Password"));
-            var loginButton = webDriver.FindElement(By.ClassName("btn-default"));
+            var txtUserName = driver.FindElement(By.Id("UserName"));
+            var txtPassword = driver.FindElement(By.Name("Password"));
+            var loginButton = driver.FindElement(By.ClassName("btn-default"));
+            var newUserLink = driver.FindElement(By.LinkText("Register as a new user"));
+            var hostingLink = driver.FindElement(By.PartialLinkText("hosting by"));
+            var rememberBox = driver.FindElement(By.XPath("//input[@name='RememberMe']"));
 
             Assert.That(txtUserName.Displayed, Is.True);
             Assert.That(txtPassword.Displayed, Is.True);
             Assert.That(loginButton.Displayed, Is.True);
+            Assert.That(newUserLink.Displayed, Is.True);
+            Assert.That(hostingLink.Displayed, Is.True);
+            Assert.That(rememberBox.Displayed, Is.True);
 
             txtUserName.SendKeys("admin");
             txtPassword.SendKeys("password");
-            //webDriver.FindElement(By.Id("RememberMe")).Click();
-            //Thread.Sleep(5000);
+            rememberBox.Click();
+            Thread.Sleep(1000);
+            rememberBox.Click();
+            Thread.Sleep(3000);
             loginButton.Click();
         }
     }
