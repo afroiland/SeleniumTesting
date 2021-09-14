3b84574378abae2cf9e98e975d4d4d4470f514c1
afroiland <andrew.froiland@gmail.com>
2021-09-13 21:09:48 -0500

    add pageload timeout, css selectors

--- SeleniumTest2/UnitTest1.cs
+++ SeleniumTest2/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using System.Threading;
 
 namespace SeleniumTest2
@@ -21,6 +22,7 @@ namespace SeleniumTest2
             driver.Navigate().GoToUrl("http://eaapp.somee.com/");
             //driver.Navigate().GoToUrl("http://localhost:3000/");
 
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(40);
             driver.Manage().Window.Maximize();
 
             IWebElement lnkLogin = driver.FindElement(By.Id("loginLink"));
@@ -32,6 +34,9 @@ namespace SeleniumTest2
             var newUserLink = driver.FindElement(By.LinkText("Register as a new user"));
             var hostingLink = driver.FindElement(By.PartialLinkText("hosting by"));
             var rememberBox = driver.FindElement(By.XPath("//input[@name='RememberMe']"));
+            var chkboxByCSS = driver.FindElement(By.CssSelector("#RememberMe"));
+            //var loginByCSS = driver.FindElement(By.CssSelector(".btn.btn-default"));
+            var loginByCSS = driver.FindElement(By.CssSelector("input[class*='efault']"));
 
             Assert.That(txtUserName.Displayed, Is.True);
             Assert.That(txtPassword.Displayed, Is.True);
@@ -39,14 +44,18 @@ namespace SeleniumTest2
             Assert.That(newUserLink.Displayed, Is.True);
             Assert.That(hostingLink.Displayed, Is.True);
             Assert.That(rememberBox.Displayed, Is.True);
+            Assert.That(chkboxByCSS.Displayed, Is.True);
+            Assert.That(loginByCSS.Displayed, Is.True);
 
             txtUserName.SendKeys("admin");
             txtPassword.SendKeys("password");
             rememberBox.Click();
             Thread.Sleep(1000);
             rememberBox.Click();
-            Thread.Sleep(3000);
-            loginButton.Click();
+            Thread.Sleep(2000);
+            loginByCSS.Click();
+            Thread.Sleep(2000);
+            driver.Close();
         }
     }
 }
